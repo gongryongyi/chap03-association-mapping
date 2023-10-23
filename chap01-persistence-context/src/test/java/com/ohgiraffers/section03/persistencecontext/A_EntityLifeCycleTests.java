257dc37e088e3ae3a783ee35d20d638acbf57155
blimu <sheep.ohgiraffers@gmail.com>
2023-10-23 14:59:44 +0900

    commit

--- chap01-persistence-context/src/test/java/com/ohgiraffers/section03/persistencecontext/A_EntityLifeCycleTests.java
+++ chap01-persistence-context/src/test/java/com/ohgiraffers/section03/persistencecontext/A_EntityLifeCycleTests.java
@@ -94,8 +94,87 @@ public class A_EntityLifeCycleTests {
         assertEquals("메론죽", foundMenu.getMenuName());
     }
 
+    @Test
+    public void 준영속성_detach_테스트() {
+        //given
+        Menu foundMenu1 = entityManager.find(Menu.class, 11);
+        Menu foundMenu2 = entityManager.find(Menu.class, 12);
+        //when
+        entityManager.detach(foundMenu2);
+        foundMenu1.setMenuPrice(5000);
+        foundMenu2.setMenuPrice(5000);
+        //then
+        assertEquals(5000, entityManager.find(Menu.class, 11).getMenuPrice());
+        assertEquals(5000, entityManager.find(Menu.class, 12).getMenuPrice());
+    }
 
+    @Test
+    public void 준영속성_clear_테스트() {
+        //given
+        Menu foundMenu1 = entityManager.find(Menu.class, 11);
+        Menu foundMenu2 = entityManager.find(Menu.class, 12);
+        //when
+        entityManager.clear();
+        foundMenu1.setMenuPrice(5000);
+        foundMenu2.setMenuPrice(5000);
+        //then
+        assertEquals(5000, entityManager.find(Menu.class, 11).getMenuPrice());
+        assertEquals(5000, entityManager.find(Menu.class, 12).getMenuPrice());
+    }
 
+    @Test
+    public void 준영속성_close_테스트() {
+        //given
+        Menu foundMenu1 = entityManager.find(Menu.class, 11);
+        Menu foundMenu2 = entityManager.find(Menu.class, 12);
+        //when
+        entityManager.close();
+        foundMenu1.setMenuPrice(5000);
+        foundMenu2.setMenuPrice(5000);
+        //then
+        assertEquals(5000, entityManager.find(Menu.class, 11).getMenuPrice());
+        assertEquals(5000, entityManager.find(Menu.class, 12).getMenuPrice());
+    }
+
+    @Test
+    public void 삭제_remove_테스트() {
+        //given
+        Menu foundMenu = entityManager.find(Menu.class, 2);
+        //when
+        entityManager.remove(foundMenu);
+        Menu refoundMenu = entityManager.find(Menu.class, 2);
+        //then
+        assertEquals(2, foundMenu.getMenuCode());
+        assertEquals(null, refoundMenu);
+    }
+
+    @Test
+    public void 병합_merge_수정_테스트() {
+        //given
+        Menu menuToDetach = entityManager.find(Menu.class, 2);
+        entityManager.detach(menuToDetach);
+        //when
+        menuToDetach.setMenuName("수박죽");
+        Menu refoundMenu = entityManager.find(Menu.class, 2);
+        entityManager.merge(menuToDetach);
+        //then
+        Menu mergedMenu = entityManager.find(Menu.class, 2);
+        assertEquals("수박죽", mergedMenu.getMenuName());
+    }
+
+    @Test
+    public void 병합_merge_삽입_테스트() {
+        //given
+        Menu menuToDetach = entityManager.find(Menu.class, 2);
+        entityManager.detach(menuToDetach);
+        //when
+        menuToDetach.setMenuCode(999);  // DB에서 조회할 수 없는 키 값으로 변경
+        menuToDetach.setMenuName("수박죽");
+        entityManager.merge(menuToDetach);  //영속 상태의 엔티티와 병합해야 하지만 존재하지 않을 경우 삽입 된다.
+        //then
+        Menu mergedMenu = entityManager.find(Menu.class, 999);
+        assertEquals("수박죽", mergedMenu.getMenuName());
+    }
 
 
 
